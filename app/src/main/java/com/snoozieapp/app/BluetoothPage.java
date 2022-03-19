b818014e8c3e8236c1eb723a59069799bb6d53a1
Kevin Perez <kevinperez@knights.ucf.edu>
2022-03-19 17:23:10 -0400

    Added Bluetooth Compatibility

---
+++ app/src/main/java/com/snoozieapp/app/BluetoothPage.java
@@ -0,0 +1,141 @@
+package com.snoozieapp.app;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Set;
+
+public class BluetoothPage extends AppCompatActivity {
+
+    private static final int REQUEST_ENABLE_BT = 0;
+    private static final int REQUEST_DISCOVER_BT = 1;
+
+    TextView mStatusBlueTv, mPairedTv;
+    ImageView mBlueIv;
+    Button mOnBtn, mOffBtn, mDiscoverBtn, mPairedBtn;
+
+    BluetoothAdapter mBlueAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bluetooth_page);
+
+        mStatusBlueTv = findViewById(R.id.statusBluetoothTv);
+        mPairedTv = findViewById(R.id.pairedTv);
+        mBlueIv = findViewById(R.id.bluetoothIv);
+        mOnBtn = findViewById(R.id.onBtn);
+        mOffBtn = findViewById(R.id.offBtn);
+        mDiscoverBtn = findViewById(R.id.discoverableBtn);
+        mPairedBtn = findViewById(R.id.pairedBtn);
+
+        //adapter
+        mBlueAdapter = BluetoothAdapter.getDefaultAdapter();
+
+//        // Check if Bluetooth is available
+//        if (mBlueAdapter == null) {
+//            mStatusBlueTv.setText("Bluetooth is not available");
+//        }
+//        else {
+//            mStatusBlueTv.setText("Bluetooth is available");
+//        }
+
+        // set image according to Bluetooth Status
+        if(mBlueAdapter.isEnabled()) {
+            mBlueIv.setImageResource(R.drawable.ic_action_on);
+        }
+        else {
+            mBlueIv.setImageResource(R.drawable.ic_action_off);
+        }
+
+        mOnBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (!mBlueAdapter.isEnabled()) {
+                    showToast("Turning on Bluetooth...");
+                    // Intent to on Bluetooth
+                    Intent intent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+                    startActivityForResult(intent, REQUEST_ENABLE_BT);
+                }
+                else {
+                    showToast("Bluetooth is on");
+                }
+
+            }
+        });
+
+        mDiscoverBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (!mBlueAdapter.isDiscovering()) {
+                    showToast("Making Devices Discoverable");
+                    Intent intent = new Intent(BluetoothAdapter.ACTION_REQUEST_DISCOVERABLE);
+                    startActivityForResult(intent, REQUEST_DISCOVER_BT);
+                }
+            }
+        });
+
+        mOffBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (mBlueAdapter.isEnabled()) {
+                    mBlueAdapter.disable();
+                    showToast("Turning Bluetooth Off");
+                    mBlueIv.setImageResource(R.drawable.ic_action_off);
+                }
+                else {
+                    showToast("Bluetooth is off");
+                }
+            }
+        });
+
+        mPairedBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (mBlueAdapter.isEnabled()) {
+                    mPairedTv.setText("Paired Devices");
+                    Set<BluetoothDevice> devices = mBlueAdapter.getBondedDevices();
+                    for (BluetoothDevice device: devices) {
+                        mPairedTv.append("\nDevice: " + device.getName() + "," + device);
+                    }
+                }
+                else {
+                    // Bluetooth is off so we can't get paired devices
+                    showToast("Turn on Bluetooth to get paired devices");
+                }
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        switch (requestCode) {
+            case REQUEST_ENABLE_BT:
+                if(resultCode == RESULT_OK) {
+                    //Bluetooth is on
+                    mBlueIv.setImageResource(R.drawable.ic_action_on);
+                    showToast("Bluetooth is on");
+                }
+                else {
+                    // User denied to turn Bluetooth on
+                    showToast("Could not connect to Bluetooth");
+                }
+                break;
+        }
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    private void showToast(String msg) {
+        Toast.makeText(this, msg, Toast.LENGTH_SHORT).show();
+    }
+}
--- app/src/main/java/com/snoozieapp/app/Settings.java
+++ app/src/main/java/com/snoozieapp/app/Settings.java
@@ -1,5 +1,6 @@
 package com.snoozieapp.app;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,13 +8,30 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 public class Settings extends Fragment {
+    private Button button;
+    private View view;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.settings_page, container, false);
+
+        button = (Button) view.findViewById(R.id.bluetoothBtn);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openBluetoothPage();
+            }
+        });
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.settings_page, container, false);
+        return view;
+    }
+    public void openBluetoothPage() {
+        Intent intent = new Intent(getView().getContext(), BluetoothPage.class);
+        startActivity(intent);
     }
 }
